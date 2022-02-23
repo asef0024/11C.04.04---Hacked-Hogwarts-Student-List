d62672857e91211d64e806a03e823aa3e9a534e7
asef0024 <asef0024@stud.kea.dk>
2022-02-23 23:33:13 +0100

    adding filtering + sorting + popup evetlistner

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 "use strict";
 
+const HTML = {}; 
 const allStudents= [];
 let studentData;
+let filteredStudents;
 
 const studentArray = {
     firstName: "",
@@ -18,23 +20,31 @@ const theJsonfile = "https://petlatkea.dk/2021/hogwarts/students.json";
 window.addEventListener("DOMContentLoaded", start);
 
 function start() {
-    console.log("start");
+    // make buttons into variables
+    HTML.allFilterBtn = document.querySelectorAll("[data-action=filter]");
+    HTML.allSortBtn = document.querySelectorAll("[data-action=sort]");
+      // Add event-listeners to btn and run the filter animal list
+    HTML.allFilterBtn.forEach((btn) => {
+        btn.addEventListener("click", filterTheList);
+    });
+    HTML.allSortBtn.forEach((btn) => {
+        btn.addEventListener("click", sortTheList);
+    });
+
     loadJSON();
 };
 
 async function loadJSON() {
-    console.log("loading Json");
     const jsonData = await fetch(theJsonfile);
     studentData = await jsonData.json();
-    console.table(studentData);
+    
     cleaningUpJson();
 };
 
 function cleaningUpJson() {
-    console.log("am I cleaning?");
-    studentData.forEach((arrayObject) => {
+    studentData.forEach(arrayObject => {
         const student = Object.create(studentArray);
-
+// console.log(student)
         let fullName = arrayObject.fullname.trim();
         let house = arrayObject.house.trim();
         let gender = arrayObject.gender.trim();
@@ -72,10 +82,74 @@ function cleaningUpJson() {
         student.house = house.charAt(0).toUpperCase() + house.substring(1).toLowerCase();
 
         allStudents.push(student);
+        displayStudent(student);
+        
     });
-    showAllStudents();
+    
 };
 
-function showAllStudents() {
-    console.table(allStudents)
-}
+function displayStudent(student) {
+    // document.querySelector("#list tbody").innerHTML = "";
+
+    const clone = document.querySelector("template#student").content.cloneNode(true);
+
+    clone.querySelector("[data-field=first_name]").textContent = student.firstName;
+    clone.querySelector("[data-field=last_name]").textContent = student.lastName;
+    clone.querySelector("[data-field=house]").textContent = student.house;
+
+    clone.querySelector(".popup_button").addEventListener("click", openPopup);
+    document.querySelector("#list tbody").appendChild( clone );
+    
+};
+
+
+function filterTheList(){
+    
+    //hent datasæt fra klikket knap
+    let filterType = this.dataset.filter
+    if (filterType === "*"){
+       filteredStudents = allStudents;
+       
+    }else{
+        filteredStudents = allStudents.filter(isStudentFilter);
+  
+        function isStudentFilter(student) {
+            if(student.filterType === filterType){
+               return true; 
+            } else {
+               return false;
+            };
+        };
+    };
+    displayStudent(filteredStudents)
+};
+
+function sortTheList(){
+    
+    //hent datasæt fra klikket knap
+    let sortFilter = this.dataset.sort;
+ 
+    filteredStudents.sort(sortByValue);
+
+    function sortByValue(a,b){
+        if (a[sortFilter] < b[sortFilter]){
+            
+            return -1;
+        } else {
+            return 1;
+            
+        }
+    };
+  displayStudent(filteredStudents);
+};
+
+function openPopup() {
+    console.log("open sesame");
+    const popup = document.querySelector(".popup");
+    popup.style.display = "block";
+};
+document.querySelector(".closePopup").addEventListener("click", CloseThePopup);
+
+function CloseThePopup(){
+    document.querySelector(".popup").style.display = "none";
+  };
